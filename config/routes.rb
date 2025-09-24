4995d620972360e98702d78d48d363f0535f435c
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-09-24 01:39:20 +0000

    [Finish] ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  resources :book, only: [:new, :create, :index, :show]
-  get 'homes/top'
-  devise_for :users
   root to: 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :create, :index, :show]
 end
