f0da9b497230f8ce52ab6c6120a24314ade9ce8b
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-09-25 22:42:20 +0000

    [Finish] 名前でログインできるように設定、ログインとログアウト後の遷移先設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, uniqueness: true
+
+  def self.find_for_database_authentication(warden_conditions)
+    name = warden_conditions[:name] || warden_conditions["name"]
+    where(name: name).first
+  end
+
   has_many :books, dependent: :destroy
+  
 end
