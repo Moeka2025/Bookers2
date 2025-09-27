2e941aee82b1de815cbcd2cdea72b7cb0ea2c76e
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-09-27 15:58:35 +0000

    [Finish] indexの左側ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,9 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
 
+  has_one_attached :profile_image
+
+  has_many :books, dependent: :destroy
+
   def self.find_for_database_authentication(warden_conditions)
     name = warden_conditions[:name] || warden_conditions["name"]
     where(name: name).first
   end
-
-  has_many :books, dependent: :destroy
-  
 end
