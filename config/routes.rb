a0602d57c536e58ee89d4e35dd8f1fff6b0e0bb6
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-09-30 06:27:52 +0000

    [Finish] edit機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/about', to: 'homes#about', as: 'about'
   devise_for :users
-  resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
