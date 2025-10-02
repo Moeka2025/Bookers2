d32b93b38007f0d7a06acfd30d886412fbec211c
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-10-02 05:36:00 +0000

    [Finish] userのshowページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
   def show
     @user = User.find(params[:id])
-    @book = @user.books
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
